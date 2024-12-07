5a3b12759d8ab14a0f0f8b80a73c55c880ef7b03
kkotburi <128236313+kkotburi@users.noreply.github.com>
2024-12-07 22:30:58 +0900

    Feat/layout

---
+++ app/posts/layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PostsLayout = ({ children }) => {
+  return (
+    <>
+      <h1>PostsLayout</h1>
+      <div>{children}</div>
+    </>
+  );
+};
+
+export default PostsLayout;
